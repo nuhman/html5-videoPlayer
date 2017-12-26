cc085b7367c89cf4ecedc16e4031ecb14b572117
nuhman <nuhman7@outlook.com>
2017-12-27 00:05:50 +0530

    change styles

--- app.js
+++ app.js
@@ -1,10 +1,31 @@
 const player = document.querySelector('.player');
 const video = player.querySelector('.video_player');
 const progress = player.querySelector('.progress');
-const progressBar = player.querySelector('.progress_filled');
+const progressBar = progress.querySelector('#progress');
 const toggle = player.querySelector('.toggle-btn');
+const toggleIcon = toggle.querySelector('.icon');
+const muteButton = player.querySelector('.mute-btn');
+const stopButton = player.querySelector('.stop-btn');
 const skipButtons = player.querySelectorAll('.skip-btn');
-const ranges = player.querySelectorAll('.player_slider');
+
+// fallback for browsers with no video player support
+let supportsProgress = (document.createElement('progress').max !== undefined);
+if (!supportsProgress) progress.setAttribute('data-state', 'fake');
+
+video.addEventListener( "loadedmetadata", function (e) {
+    let videoHeight = this.clientHeight - 67;   
+    player.querySelector('.player_controls').style.marginTop = videoHeight + "px";
+
+    let browserWidth = window.innerWidth || document.documentElement.clientWidth || document.getElementsByTagName('body')[0].clientWidth;        
+    console.log(browserWidth);
+    let videoWidth = 820 || this.videoWidth;
+    console.log(videoWidth);
+    let margin = videoWidth > browserWidth ? 0 : (browserWidth - videoWidth) /2;
+    console.log(margin);    
+    player.style.marginLeft = margin + "px";
+}, false );
+
+
 
 function togglePlay(){
     if(video.paused) video.play();
@@ -12,20 +33,23 @@ function togglePlay(){
 }
 
 function updateToggleStatus(){    
-    toggle.textContent = this.paused ? 'PLAY' : 'PAUSE';
+
+    if(this.paused){
+        toggleIcon.classList.remove('ion-pause');
+        toggleIcon.classList.add('ion-play');
+    } else {        
+        toggleIcon.classList.remove('ion-play');
+        toggleIcon.classList.add('ion-pause');
+    }        
 }
 
 function skip(){    
     video.currentTime += parseFloat(this.dataset.skip);
 }
 
-function handleRangeUpdate(){
-    video[this.name] = this.value;
-}
-
 function handleProgress(){
-    const percent = (video.currentTime / video.duration) * 100;
-    progressBar.style.width = `${percent}%`;
+    const percent = (video.currentTime / video.duration) * 100;    
+    progressBar.value = percent;
 }
 
 function seek(e){
@@ -33,16 +57,25 @@ function seek(e){
     video.currentTime = seekTime;
 }
 
+function stop(){
+    video.pause();
+    video.currentTime = 0;
+}
+
+
 video.addEventListener('click',  togglePlay);
 video.addEventListener('play',  updateToggleStatus);
 video.addEventListener('pause',  updateToggleStatus);
+video.addEventListener('stop',  stop);
 video.addEventListener('timeupdate',  handleProgress);
 
 toggle.addEventListener('click',  togglePlay);
+stopButton.addEventListener('click',  stop );
+muteButton.addEventListener('click',  () => video.muted = !video.muted );
+
 
 skipButtons.forEach(button => button.addEventListener('click', skip));
 
-ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
 
 progress.addEventListener('click', seek);
 let mousedown = false;
